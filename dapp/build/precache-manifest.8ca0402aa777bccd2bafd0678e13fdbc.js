e9ccc856379858289347cf1641da1aa05750c305
AymericNoel <aymeric.noel@edu.devinci.fr>
2020-03-17 11:22:46 +0100

    ajout readme

---
+++ dapp/build/precache-manifest.8ca0402aa777bccd2bafd0678e13fdbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "427ec8cd911bdd63e43f",
+    "url": "/static/js/main.427ec8cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "0d515b91fb7a9be70d62",
+    "url": "/static/css/2.fdc54f42.chunk.css"
+  },
+  {
+    "revision": "0d515b91fb7a9be70d62",
+    "url": "/static/js/2.0d515b91.chunk.js"
+  },
+  {
+    "revision": "e4a3fd43452fb1189d50e4f912388774",
+    "url": "/index.html"
+  }
+];
